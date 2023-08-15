92b40d0151fc60fd52a132d0623e6523a6d4dfec
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-08-15 13:16:52 +0530

    [Authentication-service] Refactor. Error constants

--- authentication-service/utils/constants/constants.go
+++ authentication-service/utils/constants/constants.go
@@ -1,9 +1,12 @@
 package constants
 
 const (
-	Success              = "success"
-	Error                = "error"
-	UP                   = "Authentication serice is UP!"
+	Success = "success"
+	Error   = "error"
+	UP      = "Authentication serice is UP!"
+)
+
+const (
 	ErrUserAlreadyExists = "user has already exists with username"
 	ErrUserNotFound      = "user is not found"
 	ErrPasswordMismatch  = "user password is incorrect"
