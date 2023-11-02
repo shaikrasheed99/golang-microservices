404ac4d820c95a33d1ef6ebc7875ce2abc4105cb
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-11-02 23:13:25 +0530

    [Frontend] Add. Navbar layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css"
+import Navbar from "./navbar/Navbar"
 
 function App() {
   return (
     <div className="container">
       <div className="header">Microservices Applicaiton</div>
+      <Navbar />
     </div>
   )
 }
---
+++ frontend/src/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div className="btn-container">
+            <button className="btn">Signup</button>
+            <button className="btn">Login</button>
+        </div>
+    );
+}
+ 
+export default Navbar;
